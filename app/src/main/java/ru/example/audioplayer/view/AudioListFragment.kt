1a38206cd26fb84bf93c8d630a393219c6d5b131
beer <kipa080@yahoo.com>
2022-08-17 17:27:49 +0300

    Плеер работает, осталось сделать: -Проигрывание песни в шторке при свернутом приложении -Добавление музыки из телефона

--- app/src/main/java/ru/example/audioplayer/view/AudioListFragment.kt
+++ app/src/main/java/ru/example/audioplayer/view/AudioListFragment.kt
@@ -6,49 +6,93 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import ru.example.audioplayer.R
 import ru.example.audioplayer.databinding.FragmentAudioListBinding
 
 
 class AudioListFragment : Fragment() {
 
-    private lateinit var binding :  FragmentAudioListBinding
-    private  var audioPlayer :   MediaPlayer? = null
-    private var currentMusic = mutableListOf(R.raw.dota2rampa_88c162695170679)
+    private lateinit var binding : FragmentAudioListBinding
+    private lateinit var audioPlayer : MediaPlayer
+    private var currentMusic = mutableListOf(R.raw.skillet_herro)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        audioPlayer = MediaPlayer.create(requireContext(), currentMusic[0])
         binding = FragmentAudioListBinding.inflate(layoutInflater)
 
-        audioPlayer = MediaPlayer.create(requireContext(), currentMusic[0])
-
         controlAudioPlayer()
+        binding.audioSeekbar.apply {
+            max = audioPlayer.duration
+            progress = 0
 
-
+        }
 
 
         return binding.root
     }
 
-    fun controlAudioPlayer(){
 
+
+    fun controlAudioPlayer(){
         binding.playerPlay.apply{
             setOnClickListener {
-                if(!audioPlayer!!.isPlaying){
-                   audioPlayer?.start()
+                if(!audioPlayer.isPlaying){
+                   audioPlayer.start()
                     setBackgroundResource(R.drawable.ic_player_pause)
 
-
             }else{
-                audioPlayer?.pause()
+                audioPlayer.pause()
                 setBackgroundResource(R.drawable.ic_player_play)
             }
 
         }
+            binding.audioSeekbar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                override fun onProgressChanged(p0: SeekBar?, position: Int, changed: Boolean) {
+                    if(changed){
+                        audioPlayer.seekTo(position)
+                    }
+                }
+
+                override fun onStartTrackingTouch(p0: SeekBar?) {
+
+                }
+
+                override fun onStopTrackingTouch(p0: SeekBar?) {
+
+                }
+
+            })
+            lifecycleScope.launch {
+                 while (true) {
+                     binding.audioSeekbar.progress = audioPlayer.currentPosition
+                     delay(100)
+
+                audioPlayer.setOnCompletionListener {
+                    binding.playerPlay.setBackgroundResource(R.drawable.ic_player_play)
+
+                }
+                 }
+            }
+
         }
+
+
+
+
     }
+
+
+
+
+
 }
 
 
